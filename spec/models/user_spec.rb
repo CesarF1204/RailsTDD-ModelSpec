aafc2a1f6138764da74fe448d44c220392eb1ec6
PrinsipeCes <princexcesar@gmail.com>
2021-07-09 19:39:31 +0800

    initialization

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  context "With valid attributes" do 
+    it "should save" do 
+      user = User.new(
+        first_name: 'Cesar',
+        last_name: 'Francisco',
+        email: 'princexcesar@gmail.com'
+      )
+      expect(user).to be_valid
+    end
+  end
+  
+  context "With invalid attributes" do 
+    it "should not save if first_name field is blank" do
+        user = User.new(
+            first_name: "", 
+            last_name: 'Francisco', 
+            email: 'princexcesar@gmail.com')
+      expect(user).to be_invalid
+    end
+
+    it "should not save if last_name field is blank" do
+      user = User.new(
+            first_name: "Cesar",
+            last_name: "",
+            email: "princexcesar@gmail.com"
+      )
+    end
+    
+    it "should not save if email already exists" do
+      user = User.new(
+        first_name: "Cesar",
+        last_name: "Francisco",
+        email: "princexcesar@gmail.com"
+      )
+      expect(user).to be_valid
+    end
+    
+    it "should contain a valid email" do
+      user = User.new(
+        first_name: "Cesar",
+        last_name: "Francisco",
+        email: "princexcesargmailcom"
+      )
+      expect(user).to be_invalid
+    end
+  end
+end
