505c6a418474c457b932f531e977bc4dbd6a0bb8
PrinsipeCes <princexcesar@gmail.com>
2021-07-12 16:46:19 +0800

    fix test email already exists

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
   end
-  
   context "With invalid attributes" do 
     it "should not save if first_name field is blank" do
         user = User.new(
@@ -19,7 +18,6 @@ RSpec.describe User, type: :model do
             email: 'princexcesar@gmail.com')
       expect(user).to be_invalid
     end
-
     it "should not save if last_name field is blank" do
       user = User.new(
             first_name: "Cesar",
@@ -27,16 +25,21 @@ RSpec.describe User, type: :model do
             email: "princexcesar@gmail.com"
       )
     end
-    
     it "should not save if email already exists" do
+      
+      expect(user).to be_valid
       user = User.new(
         first_name: "Cesar",
         last_name: "Francisco",
         email: "princexcesar@gmail.com"
       )
-      expect(user).to be_valid
+      expect(user).to be_invalid
+      # user1 = User.new(
+      #   first_name: "Hello",
+      #   last_name: "World",
+      #   email: "hello@gmail.com"
+      # )
     end
-    
     it "should contain a valid email" do
       user = User.new(
         first_name: "Cesar",
