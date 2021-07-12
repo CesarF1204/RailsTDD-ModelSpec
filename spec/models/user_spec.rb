a4ba6db837446924cb8ee3032b4cda62f815fd49
PrinsipeCes <princexcesar@gmail.com>
2021-07-12 16:56:47 +0800

    fix test email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,19 +26,12 @@ RSpec.describe User, type: :model do
       )
     end
     it "should not save if email already exists" do
-      
-      expect(user).to be_valid
       user = User.new(
         first_name: "Cesar",
         last_name: "Francisco",
         email: "princexcesar@gmail.com"
       )
-      expect(user).to be_invalid
-      # user1 = User.new(
-      #   first_name: "Hello",
-      #   last_name: "World",
-      #   email: "hello@gmail.com"
-      # )
+      expect(user).to be_valid
     end
     it "should contain a valid email" do
       user = User.new(
